0d97e5e61d1415604c0ad2b4d31d18bdbc00ca1d
weichboldg <weichboldg@gmail.com>
2026-05-26 18:15:05 +0200

    feat(version): bump to v1.15.0 (synclog rollout)

--- IDEALAKEWMSService/AppVersion.cs
+++ IDEALAKEWMSService/AppVersion.cs
@@ -2,6 +2,6 @@ namespace IDEALAKEWMSService;
 
 public static class AppVersion
 {
-    public const string Version = "1.14.0";
-    public const string Date = "2026-05-22";
+    public const string Version = "1.15.0";
+    public const string Date = "2026-05-26";
 }
