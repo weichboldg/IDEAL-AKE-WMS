2907c51d60a1f76ce283f584ce795497b9ae90bd
weichboldg <weichboldg@gmail.com>
2026-05-27 08:37:25 +0200

    feat(version): bump to v1.15.1 (activity-log rollout)

--- IDEALAKEWMSService/AppVersion.cs
+++ IDEALAKEWMSService/AppVersion.cs
@@ -2,6 +2,6 @@ namespace IDEALAKEWMSService;
 
 public static class AppVersion
 {
-    public const string Version = "1.15.0";
-    public const string Date = "2026-05-26";
+    public const string Version = "1.15.1";
+    public const string Date = "2026-05-27";
 }
--- IdealAkeWms/AppVersion.cs
+++ IdealAkeWms/AppVersion.cs
@@ -2,6 +2,6 @@ namespace IdealAkeWms;
 
 public static class AppVersion
 {
-    public const string Version = "1.15.0";
-    public const string Date = "2026-05-26";
+    public const string Version = "1.15.1";
+    public const string Date = "2026-05-27";
 }
